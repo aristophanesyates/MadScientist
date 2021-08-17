f570d405f27ed8c9bb7073d0c61db2caa406cbfa
aristophanesyates <56827855+aristophanesyates@users.noreply.github.com>
2021-08-17 16:51:46 +0100

    New Arms! New Storage. New Version!

---
+++ Assets/BodyPartCooler.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPartCooler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<GameObject> Part;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
